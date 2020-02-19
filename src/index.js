548cba4a7682ab0fc52ff69d2eb48b537b56df53
Alexander <snarsky93@gmail.com>
2020-02-19 22:42:29 +0300

    feat: add decision of this task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array || array.length == 0)
+   return 0;
+ 
+ let result =  array.sort(function(a,b){ 
+    if (a > b) 
+      return 1;
+    
+    if (a < b) 
+     return -1;
+    
+     return 0;
+  });
+ 
+    return result[0];
+ }
+ 
 
 exports.max = function max (array) {
+  if (!array || array.length == 0)
   return 0;
+
+let result =  array.sort(function(a,b){ 
+   if (a > b)
+    return 1;
+  
+  if (a < b)
+    return -1;
+  
+    return 0;
+ });
+
+   return result[result.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+
+  return  array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
 }
